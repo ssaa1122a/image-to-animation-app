3ea00c67f420cf834aada956fecb9a2a12817844
sandeep <adhikarisandeep488@gmail.com>
2025-05-15 16:46:29 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+git init
+git remote add origin https://github.com/YOUR_USERNAME/image-to-animation-app.git
+git add .
+git commit -m "Initial commit"
+git push -u origin master  # or main if that's the default
