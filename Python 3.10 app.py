4f6918df2897f288358dcec29174244758b80525
sandeep <adhikarisandeep488@gmail.com>
2025-05-15 20:21:07 +0530

    Update Python 3.10 app.py

--- Python 3.10 app.py
+++ Python 3.10 app.py
@@ -1,5 +1,27 @@
-git init
-git remote add origin https://github.com/YOUR_USERNAME/image-to-animation-app.git
-git add .
-git commit -m "Initial commit"
-git push -u origin master  # or main if that's the default
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return redirect(url_for('index'))
+    file = request.files['image']
+    if file.filename == '':
+        return redirect(url_for('index'))
+    if file:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        return render_template('result.html', image_url=filepath)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
